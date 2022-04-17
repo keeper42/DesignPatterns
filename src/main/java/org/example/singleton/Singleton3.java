8ed6595c730f3dcfb6f98372976d1d527eb0ec38
keeper42 <szuljf@163.com>
2022-04-17 16:31:40 +0800

    combination

--- src/main/java/org/example/singleton/Singleton3.java
+++ src/main/java/org/example/singleton/Singleton3.java
@@ -8,7 +8,7 @@ package org.example.singleton;
  * 饿汉式
  * 优点：没有锁，执行效率会提高。
  * 缺点：类加载时就初始化，浪费内存。
- * 它基于 classloader 机制避免了多线程的同步问题。
+ * 它基于 classloader 机制避免了多线程的同步问题，单例对象在类装载时就实例化了。
  */
 public class Singleton3 {
 
